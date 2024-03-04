178f973be8479df4380a99601a49e26713375fd5
Ariel Tequida <ariel.tequida@gmail.com>
2024-03-03 18:50:18 -0700

    Nothing

--- app/voice_to_text.py
+++ app/voice_to_text.py
@@ -1,6 +1,5 @@
 import whisper
 
-
 # model = whisper.load_model(name='small', download_root='./models/')  # Use to download models
 model = whisper.load_model('./models/small.pt')
 result = model.transcribe('./audio_grabado.wav', fp16=False)
